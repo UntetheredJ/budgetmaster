2af1bb91ef00f6ab3848e60c0bbeab674e6ade31
LRC25 <laura2504rc@gmail.com>
2023-07-01 22:15:26 -0500

    Configuration Page
    Update password

--- budgetmaster/lib/screens/configuracion.dart
+++ budgetmaster/lib/screens/configuracion.dart
@@ -1,16 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:budgetmaster/screens/login_screen.dart';
 import 'package:budgetmaster/models/usuario.dart';
 import 'package:budgetmaster/db/supabaseConnection.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 class Configuracion extends StatelessWidget {
   var newUsuarioController = TextEditingController();
+  var newcontrasenna1Controller = TextEditingController();
+  var newcontrasenna2Controller = TextEditingController();
+  var contrasennaController = TextEditingController();
   final Usuario usuario;
   Configuracion({Key? key ,required this.usuario}) : super(key: key);
 
   // Iniciar instancia de base de datos
   final SupabaseService _supabaseService = SupabaseService();
   SupabaseClient get cliente => _supabaseService.client;
+
+
+
 
 @override
 Widget build(BuildContext context) {
@@ -44,7 +51,35 @@ Widget build(BuildContext context) {
               // Contents
               children: [
 
-                     ElevatedButton.icon(
+                ElevatedButton.icon(
+                  icon:  const Icon(Icons.person_outline_sharp, size: 18),
+                  label: const Text('Nombre'),
+                  onPressed: () {showDialog(context: context, builder: (BuildContext context){
+                    return  AlertDialog(
+                      title: const Text("Nombre:", style: TextStyle(fontSize: 20, color: Colors.black, fontWeight: FontWeight.bold),),
+                      content: SingleChildScrollView(
+                        child: ListBody(
+                          children: [
+                            Text(
+                              usuario.nombre,
+                              style: const TextStyle(
+                                fontSize: 18,
+                                color: Colors.purple,
+                              ),
+                            ),
+                            const SizedBox(height: 20,),
+
+
+                          ],
+                        ),
+                      ),
+                    );
+
+                  });},
+                ),
+
+
+                ElevatedButton.icon(
                       icon:  const Icon(Icons.mail, size: 18),
                       label: const Text('Correo'),
                       onPressed: () {showDialog(context: context, builder: (BuildContext context){
@@ -163,42 +198,166 @@ Widget build(BuildContext context) {
                     ),
 
                 ElevatedButton.icon(
-                  icon:  const Icon(Icons.phone, size: 18),
-                  label: Text('Teléfono'),
-                  onPressed: () {},
-                ),
-                ElevatedButton.icon(
                   icon:  const Icon(Icons.password, size: 18),
-                  label: Text('Contraseña'),
-                  onPressed: () {},
-                ),
+                  label: const Text('Contraseña'),
+                  onPressed: () {showDialog(context: context, builder: (BuildContext context){
+                    return  AlertDialog(
+                      content: SingleChildScrollView(
+                        child: ListBody(
+                          children: [
+                            const SizedBox(height: 20,),
+                            ExpansionTile(
+                              leading:  const CircleAvatar(
+                                backgroundColor: Colors.deepPurpleAccent,
+                                child: Icon(Icons.edit, size: 20,),
+                              ),
+                              title:  const Text("Cambiar contraseña:", style: TextStyle(fontSize: 15, color: Colors.blue, fontWeight: FontWeight.bold),),
+                              // Contents
+                              children: [
+                                const SizedBox(height: 10,),
+                                const Text("Ingrese contraseña actual:", style: TextStyle(fontSize: 15, color: Colors.black, fontWeight: FontWeight.bold),),
+                                const SizedBox(height: 10,),
+                                TextFormField(
+                                  controller: contrasennaController,
+                                  keyboardType:  TextInputType.visiblePassword,
+                                  obscureText: true,
+                                  decoration: const InputDecoration(
+                                    labelText: "Contraseña",
+                                    border: OutlineInputBorder(),
+                                    prefixIcon: Icon(Icons.password),
+                                  ),
+                                ),
 
-              ],
-            ),
+                                const SizedBox(height: 10,),
+                                const Text("Ingrese nueva contraseña:", style: TextStyle(fontSize: 15, color: Colors.black, fontWeight: FontWeight.bold),),
+                                const SizedBox(height: 10,),
+                                TextFormField(
+                                  controller: newcontrasenna1Controller,
+                                  keyboardType:  TextInputType.visiblePassword,
+                                  obscureText: true,
+                                  decoration: const InputDecoration(
+                                    labelText: "Contraseña",
+                                    border: OutlineInputBorder(),
+                                    prefixIcon: Icon(Icons.password),
+                                  ),
+                                ),
+                                const SizedBox(height: 10,),
+                                const Text("Confirmar nueva contraseña:", style: TextStyle(fontSize: 15, color: Colors.black, fontWeight: FontWeight.bold),),
+                                const SizedBox(height: 10,),
+                                TextFormField(
+                                  controller: newcontrasenna2Controller,
+                                  keyboardType:  TextInputType.visiblePassword,
+                                  obscureText: true,
+                                  decoration: const InputDecoration(
+                                    labelText: "Contraseña",
+                                    border: OutlineInputBorder(),
+                                    prefixIcon: Icon(Icons.password),
+                                  ),
+                                ),
 
-            const ExpansionTile(
-              leading:  CircleAvatar(
-                backgroundColor: Colors.deepPurpleAccent,
-                child: Icon(Icons.settings, size: 20,),
-              ),
-              title: Text('Información personal'),
-              // Contents
-              children: [
-                ExpansionTile(
-                  leading:  CircleAvatar(
-                    backgroundColor: Colors.blue,
-                    child: Icon(Icons.email, size: 20,),
-                  ),
-                  title: Text('Nombres y apellidos'),
-                  // Contents
-                  children: [
-                    ListTile(
-                        leading: CircleAvatar(
-                          backgroundColor: Colors.blue,
+                                const SizedBox(height: 10,),
+                                Container(
+                                  height: 45,
+                                  width: double.infinity,
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(100),
+                                    gradient: const LinearGradient(
+                                        colors: [Colors.yellow, Colors.green],
+                                        begin: Alignment.topRight,
+                                        end: Alignment.bottomLeft),
+                                  ),
+                                  child: MaterialButton(
+                                    onPressed: () async {
+                                      String contrasenna = contrasennaController.text;
+                                      String contrasenna1 = newcontrasenna1Controller.text;
+                                      String contrasenna2 = newcontrasenna2Controller.text;
+                                      if (contrasenna == usuario.contrasenna && contrasenna1 == contrasenna2) {
+                                        int valor = await actualizarContrasenna(contrasenna1);
+
+                                      if (valor == 1) {
+                                        // ignore: use_build_context_synchronously
+                                        showDialog(
+                                          context: context,
+                                          builder: (BuildContext context) {
+                                            return AlertDialog(
+                                              title: Text("Correcto"),
+                                              content: Text("Se ha cambio su contraseña. Vuelva a iniciar sesión para visualizar los cambios"),
+                                              actions: <Widget>[
+
+                                                TextButton(
+                                                  child: Text("Ok"),
+                                                  onPressed: () {
+                                                    Navigator.pop(context);
+                                                  },
+                                                ),
+                                              ],
+                                            );
+                                          },
+                                        );
+                                      }  else {
+                                        // ignore: use_build_context_synchronously
+                                        showDialog(
+                                            context: context,
+                                            builder: (BuildContext context) {
+                                              return const AlertDialog(
+                                                title: Text("Error"),
+                                                content: SingleChildScrollView(
+                                                  child: ListBody(
+                                                    children: [
+                                                      Text("No se pudo actualizar la contraseña")
+                                                    ],
+                                                  ),
+                                                ),
+                                              );
+                                            }
+                                        );
+                                      }
+                                      }
+                                      else {
+                                        // ignore: use_build_context_synchronously
+                                        showDialog(
+                                            context: context,
+                                            builder: (BuildContext context) {
+                                              return const AlertDialog(
+                                                title: Text("Alerta"),
+                                                content: SingleChildScrollView(
+                                                  child: ListBody(
+                                                    children: [
+                                                      Text("Incosistencia en contraseña")
+                                                    ],
+                                                  ),
+                                                ),
+                                              );
+                                            }
+                                        );
+                                      }
+                                    },
+                                    child: const Text(
+                                      "Guardar cambios",
+                                      style: TextStyle(
+                                        fontSize: 20,
+                                        color: Colors.white,
+
+                                      ),
+
+                                    ),
+
+                                  ),
+
+                                ),
+                                const SizedBox(height: 10,),
+                              ],
+                            ),
+
+                          ],
                         ),
-                        title: Text('Blue')),
-                  ],
+                      ),
+                    );
+
+                  });},
                 ),
+
+
               ],
             ),
 
@@ -256,8 +415,10 @@ Widget build(BuildContext context) {
                     begin: Alignment.topRight,
                     end: Alignment.bottomLeft),
               ),
-              child: MaterialButton(
-                onPressed: () {
+
+                child: MaterialButton(
+                onPressed: () async {
+
                 },
                 child: const Text(
                   "Cerrar sesión",
@@ -298,4 +459,22 @@ Future<int> actualizarUsuario(String newusuario) async {
     return 0;
   }
 }
+
+  Future<int> actualizarContrasenna(String newcontrasenna) async {
+    try {
+      await cliente
+          .from('usuario')
+          .update({ 'contrasenna': newcontrasenna})
+          .match({ 'usuario': usuario.usuario }
+      );
+      debugPrint("Correcto, cambio de contraseña realizado");
+      return 1;
+    } catch (e) {
+      debugPrint(e.toString());
+      return 0;
+    }
+  }
 }
+
+
+
