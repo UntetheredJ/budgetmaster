1e836e91c30cf3b95544077eda52ec9e327b941f
UntetheredJ <101756457+UntetheredJ@users.noreply.github.com>
2023-07-06 18:03:59 -0500

    Update inicio_widget.dart
    Add next payment rows

--- budgetmaster/lib/widgets/inicio_widget.dart
+++ budgetmaster/lib/widgets/inicio_widget.dart
@@ -22,20 +22,145 @@ class InicioWidget extends StatelessWidget {
       );
 
   Widget buildPagos() => Container(
-        padding: const EdgeInsets.symmetric(horizontal: 30),
-        child: const Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            Center(
+            const Center(
                 child: Text("Próximos Pagos",
                     style:
                         TextStyle(fontWeight: FontWeight.bold, fontSize: 24))),
-            SizedBox(height: 12),
-            Text("¡Este es un pago próximo!"),
-            SizedBox(height: 12),
-            Text("¡Este es otro pago próximo!"),
-            SizedBox(height: 12),
-            Text("¡Este es otro pago próximo!"),
+            DataTable(
+                dividerThickness: 0,
+                dataRowMaxHeight: 80,
+                headingRowHeight: 0,
+                columns: const [
+                  DataColumn(label: Text("")),
+                  DataColumn(label: Text("")),
+                ],
+                rows: [
+                  DataRow(cells: [
+                    DataCell(
+                      Container(
+                        width: 5,
+                        height: 60,
+                        color: Colors.purple,
+                        child: const Text(""),
+                      ),
+                    ),
+                    const DataCell(Column(
+                      children: [
+                        Row(
+                          children: [
+                            Text(
+                              "07-07-2023",
+                              style: TextStyle(
+                                  fontSize: 16, fontWeight: FontWeight.normal),
+                            ),
+                          ],
+                        ),
+                        Row(children: [
+                          Text(
+                            "Arriendo",
+                            style: TextStyle(
+                                fontSize: 20, fontWeight: FontWeight.bold),
+                          ),
+                        ]),
+                        Row(
+                          children: [
+                            Text(
+                              "800.000",
+                              style: TextStyle(
+                                  fontSize: 15, color: Color(0xFF7B1FA2)),
+                            ),
+                          ],
+                        )
+                      ],
+                    )),
+                  ]),
+                  DataRow(cells: [
+                    DataCell(
+                      Container(
+                        width: 5,
+                        height: 60,
+                        color: Colors.purple,
+                        child: const Text(""),
+                      ),
+                    ),
+                    const DataCell(Column(
+                      children: [
+                        Row(
+                          children: [
+                            Text(
+                              "09-07-2023",
+                              style: TextStyle(
+                                  fontSize: 16, fontWeight: FontWeight.normal),
+                            ),
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            Text(
+                              "Movistar",
+                              style: TextStyle(
+                                  fontSize: 20, fontWeight: FontWeight.bold),
+                            ),
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            Text(
+                              "37.000",
+                              style: TextStyle(
+                                  fontSize: 15, color: Color(0xFF7B1FA2)),
+                            ),
+                          ],
+                        )
+                      ],
+                    )),
+                  ]),
+                  DataRow(cells: [
+                    DataCell(
+                      Container(
+                        width: 5,
+                        height: 60,
+                        color: Colors.purple,
+                        child: const Text(""),
+                      ),
+                    ),
+                    const DataCell(Column(
+                      children: [
+                        Row(
+                          children: [
+                            Text(
+                              "15-07-2023",
+                              style: TextStyle(
+                                  fontSize: 16, fontWeight: FontWeight.normal),
+                            ),
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            Text(
+                              "Cuota Crédito",
+                              style: TextStyle(
+                                  fontSize: 20, fontWeight: FontWeight.bold),
+                            ),
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            Text(
+                              "450.000",
+                              style: TextStyle(
+                                  fontSize: 15, color: Color(0xFF7B1FA2)),
+                            ),
+                          ],
+                        )
+                      ],
+                    )),
+                  ]),
+                ]),
           ],
         ),
       );
